88be286d098d16575d22b7ea463853ca02d2a4bf
Sergey Tvarnov <bropstk@gmail.com>
2021-12-23 00:31:33 +0200

    useState() for posts-json. Rendering json to PostItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./components/styles/App.css";
 import PostItem from "./components/PostItem.jsx";
 
 function App() {
+    const [posts, setPosts] = useState([
+        {id: 1, title: "Javascript", body: "Description"},
+        {id: 2, title: "Javascript 2", body: "Description"},
+        {id: 3, title: "Javascript 3", body: "Description"}
+    ]
+    )
+
     return (
         <div className="App">
-            <PostItem post={{id: 1, title: "Javascript", body: "Description"}} />
-            <PostItem post={{id: 1, title: "Javascript", body: "Description"}} />
-            <PostItem post={{id: 1, title: "Javascript", body: "Description"}} />
+            {posts.map(post =>
+                <PostItem post={post} key={post.id} />
+            )}
         </div>
     );
 }
