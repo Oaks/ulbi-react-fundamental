4158318f1c2a154c3900f9d5339b2537a8b39ce3
Sergey Tvarnov <bropstk@gmail.com>
2021-12-22 18:00:25 +0200

    Move post to component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import "./components/styles/App.css";
+import PostItem from "./components/PostItem.jsx";
 
 function App() {
     return (
         <div className="App">
-            <div className="post">
-                <div className="post__content">
-                    <strong>1. Javascript</strong>
-                    <div>
-                        Javascript - язык программирования
-                    </div>
-                </div>
-                <div className="post__btns">
-                    <button>Удалить</button>
-                </div>
-            </div>
+            <PostItem />
         </div>
     );
 }
