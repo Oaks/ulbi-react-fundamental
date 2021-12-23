04a123260245e6ede7211b6eb65c52489f984381
Sergey Tvarnov <bropstk@gmail.com>
2021-12-23 21:07:29 +0200

    Form: simple form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import "./components/styles/App.css";
 import PostList from './components/PostList';
+import MyButton from "./components/UI/button/MyButton.jsx";
+import MyInput from "./components/UI/input/MyInput.jsx";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -12,6 +14,11 @@ function App() {
 
     return (
         <div className="App">
+            <form>
+                <MyInput type="text" placeholder="Название поста" />
+                <MyInput type="text" placeholder="Описание поста" />
+                <MyButton disabled>Создать пост</MyButton>
+            </form>
             <PostList posts={posts} title="Список постов" />
         </div>
     );
