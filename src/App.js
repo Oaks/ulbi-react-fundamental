9e5b796064e8ded992667af82e0563fb83a68715
Sergey Tvarnov <bropstk@gmail.com>
2021-12-23 21:50:56 +0200

    Form: working form , separate

--- src/App.js
+++ src/App.js
@@ -11,13 +11,36 @@ function App() {
         {id: 3, title: "Javascript 3", body: "Description"}
     ]
     )
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPosts([...posts, newPost]);
+        setTitle("");
+        setBody("");
+    }
 
     return (
         <div className="App">
             <form>
-                <MyInput type="text" placeholder="Название поста" />
-                <MyInput type="text" placeholder="Описание поста" />
-                <MyButton disabled>Создать пост</MyButton>
+                <MyInput
+                    type="text"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    placeholder="Название поста" />
+                <MyInput
+                    type="text"
+                    value={body}
+                    onChange={e => setBody(e.target.value)}
+                    placeholder="Описание поста" />
+
+                <MyButton onClick={addNewPost} >Создать пост</MyButton>
             </form>
             <PostList posts={posts} title="Список постов" />
         </div>
