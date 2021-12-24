7aeb325b1785ef50e4cfb5e9df852028377ae1f2
Sergey Tvarnov <bropstk@gmail.com>
2021-12-24 18:26:01 +0200

    Delete post

--- src/App.js
+++ src/App.js
@@ -15,10 +15,14 @@ function App() {
         setPosts([...posts, post]);
     }
 
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
     return (
         <div className="App">
             <PostForm create={createPost} />
-            <PostList posts={posts} title="Список постов" />
+            <PostList remove={removePost} posts={posts} title="Список постов" />
         </div>
     );
 }
