2f8fd82cf3b9016a373162fee05b2a75280fabb9
Sergey Tvarnov <bropstk@gmail.com>
2021-12-26 22:04:18 +0200

    Refactoring PostList

--- src/App.js
+++ src/App.js
@@ -46,13 +46,7 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
-            {
-                sortedAndSearchedPosts.length
-                    ? <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
-                    : <h1 style={{textAlign: "center"}}>
-                        Посты не найдены
-                    </h1>
-            }
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
         </div>
     );
 }
