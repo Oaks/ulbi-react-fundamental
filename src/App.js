aa3a3fd0fcd1f65928c8f3751364112c87f54b5b
Sergey Tvarnov <bropstk@gmail.com>
2022-01-01 23:50:22 +0200

    AppRouter component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
-import About from './pages/About';
-import Posts from './pages/Posts';
-import Error from './pages/Error';
+import {BrowserRouter} from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
+import AppRouter from './components/UI/AppRouter';
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar />
-            <Switch>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/posts">
-                    <Posts />
-                </Route>
-                <Route path="/error">
-                    <Error />
-                </Route>
-                <Redirect to="/error" />
-            </Switch>
+            <AppRouter />
         </BrowserRouter>
     );
 }
