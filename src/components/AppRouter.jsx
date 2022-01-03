e6f7fe1ff47094daec922a047cad49868b0e3b7a
Sergey Tvarnov <bropstk@gmail.com>
2022-01-04 00:49:42 +0200

    Define routes array

---
+++ src/components/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Route, Switch} from 'react-router-dom';
+import {routes} from '../../router';
+
+const AppRouter = () => {
+    return (
+        <Switch>
+            {routes.map((route, index) =>
+                <Route
+                    path={route.path}
+                    component={route.component}
+                    exact={route.exact}
+                    key={index}
+                />
+            )}
+        </Switch>
+    )
+}
+
+export default AppRouter;
+
